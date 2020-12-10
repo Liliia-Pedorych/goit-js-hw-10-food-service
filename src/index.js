49ce22995babda1e0bfd2ce7c46970bab7cb0ea8
Liliia Pedorych <liliapedorych@ukr.net>
2020-12-10 22:18:30 +0200

    Додає код зміни теми

--- src/index.js
+++ src/index.js
@@ -2,7 +2,60 @@ import './styles.css';
 import itemsTemplate from './templates/menu-item.hbs';
 import menu from './menu.json';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+  };
+
 const markup = itemsTemplate(menu);
 
-const menuRef = document.querySelector('.js-menu');
-menuRef.insertAdjacentHTML('beforeend', markup);
+
+const refs = {
+    menu: document.querySelector('.js-menu'),
+    body: document.body,
+    theme: document.querySelector('#theme-switch-toggle'),
+}
+
+const savedTheme = localStorage.getItem('Theme');
+
+refs.menu.insertAdjacentHTML('beforeend', markup);
+refs.body.classList.add(Theme.LIGHT);
+
+if (savedTheme.slice(1,-1) === "dark-theme") {
+    refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    refs.theme.checked = true;
+}
+
+refs.theme.addEventListener('change', () => {
+    if (refs.body.classList.contains(Theme.LIGHT)) {
+        refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
+    } else {
+        refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+        localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
+    }
+}
+);
+
+
+
+
+
+
+
+
+
+
+// refs.theme.addEventListener('change', themeChangeHandler);
+
+
+// const themeChangeHandler = () => {
+//     if (refs.body.classList.contains(Theme.LIGHT)) {
+    //     refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    //     localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
+    // } else {
+    //     refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    //     localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
+    // }
+// }
+
